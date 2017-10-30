01a5b830616dfd6084395808814322678cc78a1a
Nielsen Puma <nielhpuma@gmail.com>
2017-10-30 18:44:17 -0500

    Adición de los métodos para Empleado en Factory.
    Se agregan los métodos necesarios para implementar el Login en Empleado.

--- SistVentasFarmacia_Cibertec/src/dao/DAOFactory.java
+++ SistVentasFarmacia_Cibertec/src/dao/DAOFactory.java
@@ -1,11 +1,15 @@
 package dao;
 
+import interfaces.EmpleadoDAO;
+
 public abstract class DAOFactory {
 	public static final int MYSQL = 1;
 	public static final int ORACLE = 2;
 	public static final int DB2 = 3;
 	public static final int SQLSERVER = 4;
 	public static final int XML = 5;
+	
+	public abstract EmpleadoDAO getEmpleado();
 	
 	public static DAOFactory getDAOFactory(int whichFactory) {
 		switch (whichFactory) {

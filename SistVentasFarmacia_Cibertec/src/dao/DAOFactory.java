fa9fe32b84c902bf0816b06573395acd0402ddf5
Nielsen Puma <nielhpuma@gmail.com>
2017-10-30 16:25:32 -0500

    Adición de los archivos JAVA
    Se agrega la implementación del patrón DAO y MVC con los archivos JAVA.

---
+++ SistVentasFarmacia_Cibertec/src/dao/DAOFactory.java
@@ -0,0 +1,25 @@
+package dao;
+
+public abstract class DAOFactory {
+	public static final int MYSQL = 1;
+	public static final int ORACLE = 2;
+	public static final int DB2 = 3;
+	public static final int SQLSERVER = 4;
+	public static final int XML = 5;
+	
+	public static DAOFactory getDAOFactory(int whichFactory) {
+		switch (whichFactory) {
+		case MYSQL:
+			return new MysqlDAOFactory();
+		case ORACLE:
+			// return new OracleDAOFactory();
+		case DB2:
+			// return new Db2DAOFactory();
+		case SQLSERVER:
+			// return new SqlServerDAOFactory();
+		case XML:
+			// return new XmlDAOFactory();
+		}
+		return null;
+	}
+}
